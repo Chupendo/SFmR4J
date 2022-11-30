79e27b37f3c6f89b4d22c7f62a002cb38cb56c34
Chupendo <andresruizpenuela@gmail.com>
2022-11-30 15:31:29 +0100

    [s3v6] init project empaty by multiplecy injection dependency

---
+++ multiple_inyection_dependecy/src/main/java/com/devs4j/mid/MultipleInyectionDependecyApplication.java
@@ -0,0 +1,13 @@
+package com.devs4j.mid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleInyectionDependecyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleInyectionDependecyApplication.class, args);
+	}
+
+}
---
+++ multiple_inyection_dependecy/src/test/java/com/devs4j/mid/MultipleInyectionDependecyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devs4j.mid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleInyectionDependecyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
