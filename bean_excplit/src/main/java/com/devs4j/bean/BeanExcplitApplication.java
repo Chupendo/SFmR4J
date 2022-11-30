f21a9050740a9e3fc2ab3450fdf54e0989d18748
Chupendo <andresruizpenuela@gmail.com>
2022-11-30 13:43:32 +0100

    [s3v5] init project empaty by definition explcit beans

---
+++ bean_excplit/src/main/java/com/devs4j/bean/BeanExcplitApplication.java
@@ -0,0 +1,13 @@
+package com.devs4j.bean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanExcplitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanExcplitApplication.class, args);
+	}
+
+}
