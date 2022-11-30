5190bf399e53ccca3526e4c3989eab5a1d891707
Chupendo <andresruizpenuela@gmail.com>
2022-11-30 11:24:01 +0100

    [s2v1] init project empaty by control_profiles

---
+++ control_prifles/src/main/java/com/devs4j/profiles/ControlPriflesApplication.java
@@ -0,0 +1,13 @@
+package com.devs4j.profiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlPriflesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlPriflesApplication.class, args);
+	}
+
+}
---
+++ control_prifles/src/test/java/com/devs4j/profiles/ControlPriflesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devs4j.profiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlPriflesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
