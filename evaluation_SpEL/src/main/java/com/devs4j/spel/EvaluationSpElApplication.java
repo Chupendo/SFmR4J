c05ae95f4bb7350e3cf1510dd12fa960ade7feec
Chupendo <andresruizpenuela@gmail.com>
2022-11-30 21:53:48 +0100

    [s3v7] init project empaty by SpEL

---
+++ evaluation_SpEL/src/main/java/com/devs4j/spel/EvaluationSpElApplication.java
@@ -0,0 +1,13 @@
+package com.devs4j.spel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationSpElApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluationSpElApplication.class, args);
+	}
+
+}
