c6fe26d44b819f4ce9f021b27eddba68df204787
Chupendo <andresruizpenuela@gmail.com>
2022-11-30 14:02:07 +0100

    [s3v5] ejemplo de definición de bean

--- bean_excplit/src/main/java/com/devs4j/bean/BeanExcplitApplication.java
+++ bean_excplit/src/main/java/com/devs4j/bean/BeanExcplitApplication.java
@@ -1,13 +1,26 @@
 package com.devs4j.bean;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BeanExcplitApplication {
+	
+	private static final Logger log = LoggerFactory.getLogger(BeanExcplitApplication.class);
 
+	@Bean
+	public static String getNombreApp() {
+		return "BeanExcplitApplication";
+	} 
 	public static void main(String[] args) {
-		SpringApplication.run(BeanExcplitApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(BeanExcplitApplication.class, args);
+		
+		String nameApp = context.getBean(String.class);
+		log.info("Bean String {} "+nameApp);
 	}
 
 }
