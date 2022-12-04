7bb5df2d82d4803175fa1134bef7e5888b6be05c
Andres Ruiz <andres.rpenuela@babelgroup.com>
2022-12-04 13:34:40 +0100

    [s4v6] init project empaty by bean postProcessor

---
+++ beanPostProcessor/src/main/java/com/devs4j/bpp/BeanPostProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.devs4j.bpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanPostProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanPostProcessorApplication.class, args);
+	}
+
+}
---
+++ beanPostProcessor/src/test/java/com/devs4j/bpp/BeanPostProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devs4j.bpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanPostProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
