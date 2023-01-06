63b275d67dc2aa1becbec7cd366d22b3ab562ac7
Andres Ruiz <andres.rpenuela@babelgroup.com>
2023-01-06 13:29:19 +0100

    endpoint para crear un usuario

--- SpringFramwerokConfiguration/src/main/java/com/devs4j/aop/controllers/UserController.java
+++ SpringFramwerokConfiguration/src/main/java/com/devs4j/aop/controllers/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +29,10 @@ class UserController {
 	@GetMapping(value = "/{username}")
 	public ResponseEntity<User> getUserByUserName(@PathVariable("username") String userName){
 		return new ResponseEntity<User>(userService.getUserByName(userName),HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> createUser(@RequestBody User  user){
+		return new ResponseEntity<User>(userService.createUser(user),HttpStatus.OK);
 	}
 }
--- SpringFramwerokConfiguration/src/main/java/com/devs4j/aop/services/UserService.java
+++ SpringFramwerokConfiguration/src/main/java/com/devs4j/aop/services/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -18,29 +19,40 @@ public class UserService {
 
 	@Autowired
 	private Faker faker;
-	
+
 	/**
 	 * Códgo para mi servicio
 	 */
 	private List<User> lusers = new ArrayList<User>();
-	
+
 	@PostConstruct
 	public void init() {
-		for (int i=0;i<100;i++) {
-			lusers.add(new User(faker.dragonBall().character(),faker.funnyName().name(),faker.pokemon().name()));
+		for (int i = 0; i < 100; i++) {
+			lusers.add(new User(faker.dragonBall().character(), faker.funnyName().name(), faker.pokemon().name()));
 		}
 	}
-	
-	public List<User> getUsers(){
+
+	public List<User> getUsers() {
 		return lusers;
 	}
-	
+
 	public User getUserByName(String userName) {
-		return lusers.stream()
-		.filter(u->u.getUsername().equals(userName))
-		.findAny()
-		.orElseThrow(()->
-			new ResponseStatusException(HttpStatus.BAD_REQUEST, 
-					String.format("User %s not found", userName)));
+		return lusers.stream().filter(u -> u.getUsername().equals(userName)).findAny()
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST,
+						String.format("User %s not found", userName)));
+	}
+
+	public User createUser(User user) {
+		boolean isExist = lusers.stream()
+				.anyMatch(u ->
+					u.getNickName().equals(user.getNickName()));
+		if (isExist) {
+			throw new ResponseStatusException(
+					HttpStatus.CONFLICT,
+					String.format("The nick %s is used", user.getNickName()));
+		}
+		
+		lusers.add(user);
+		return user;
 	}
 }
