61f4a2fd98adeb1efff0d4ae4bcf12c313d05f2d
Andres Ruiz <andres.rpenuela@babelgroup.com>
2023-01-06 11:14:19 +0100

    uso de lombok para crear una clase POJO que represente el modelo User

---
+++ SpringFramwerokConfiguration/src/main/java/com/devs4j/aop/models/User.java
@@ -0,0 +1,19 @@
+package com.devs4j.aop.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class User {
+	private String nickName;
+	private String username;
+	private String password;
+	
+}
