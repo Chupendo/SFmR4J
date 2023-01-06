3ab222c829463793ed976ff78294034a952bac97
Andres Ruiz <andres.rpenuela@babelgroup.com>
2023-01-06 10:40:45 +0100

    configuracion inicial de un proyecto en spring web

---
+++ SpringFramwerokConfiguration/src/main/java/com/devs4j/aop/SpringFramwerokConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.devs4j.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFramwerokConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFramwerokConfigurationApplication.class, args);
+	}
+
+}
---
+++ SpringFramwerokConfiguration/src/test/java/com/devs4j/aop/SpringFramwerokConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devs4j.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFramwerokConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
