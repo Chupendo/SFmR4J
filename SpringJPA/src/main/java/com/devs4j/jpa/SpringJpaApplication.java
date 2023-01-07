4888cb705bc3893d5de50116a9c44bdd31a8af18
Andres Ruiz <andres.rpenuela@babelgroup.com>
2023-01-07 19:43:22 +0100

    integración de actuator en spring

--- SpringJPA/src/main/java/com/devs4j/jpa/SpringJpaApplication.java
+++ SpringJPA/src/main/java/com/devs4j/jpa/SpringJpaApplication.java
@@ -1,6 +1,5 @@
 package com.devs4j.jpa;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
