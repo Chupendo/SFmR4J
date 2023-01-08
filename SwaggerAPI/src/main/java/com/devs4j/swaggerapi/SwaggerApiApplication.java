31430825cd23d0f13d20fbb15ed341ffe1d2078a
Chupendo <andresruizpenuela@gmail.com>
2023-01-08 18:51:18 +0100

    swagger doc
    describir methods

--- SwaggerAPI/src/main/java/com/devs4j/swaggerapi/SwaggerApiApplication.java
+++ SwaggerAPI/src/main/java/com/devs4j/swaggerapi/SwaggerApiApplication.java
@@ -1,9 +1,12 @@
 package com.devs4j.swaggerapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API DEVS4J"))
 public class SwaggerApiApplication {
 
     public static void main(String[] args) {
--- SwaggerAPI/src/main/java/com/devs4j/swaggerapi/controllers/UsersControllers.java
+++ SwaggerAPI/src/main/java/com/devs4j/swaggerapi/controllers/UsersControllers.java
@@ -1,6 +1,7 @@
 package com.devs4j.swaggerapi.controllers;
 
 import io.micrometer.core.annotation.Timed;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ public class UsersControllers {
 
     @GetMapping
     @Timed("get.users")
+    @Operation(description = "Get a list with users that start @{startWith} or all users")
     public String getUsers(){
         return "Hola world!";
     }
