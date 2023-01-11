a56142ca5ea780bc4ed45a181b3032462fb84b25
Andres Ruiz <andres.rpenuela@babelgroup.com>
2023-01-11 21:17:36 +0100

    init empaty proyect

---
+++ CachingAPI/src/main/java/com/devs4j/caching/CachingApiApplication.java
@@ -0,0 +1,13 @@
+package com.devs4j.caching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingApiApplication.class, args);
+	}
+
+}
---
+++ CachingAPI/src/test/java/com/devs4j/caching/CachingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devs4j.caching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
