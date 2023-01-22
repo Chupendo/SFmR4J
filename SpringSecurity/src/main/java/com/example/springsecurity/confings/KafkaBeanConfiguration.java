1a35b243d975e31df4f6c9a9c490356ddff3c7c6
Chupendo <andresruizpenuela@gmail.com>
2023-01-22 20:06:20 +0100

    add dependecy kafka

---
+++ SpringSecurity/src/main/java/com/example/springsecurity/confings/KafkaBeanConfiguration.java
@@ -0,0 +1,15 @@
+package com.example.springsecurity.confings;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaBeanConfiguration {
+
+    /**
+     * Producer
+     */
+
+    /**
+     * Consumer
+     */
+}
